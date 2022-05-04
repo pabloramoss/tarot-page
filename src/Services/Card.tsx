b450b95ab3bdb48e95dd6222265354fe753b9cac
Pablo Ramos <pablo-362@hotmail.com>
2022-05-03 22:03:32 -0300

    add services image animation

--- src/Services/Card.tsx
+++ src/Services/Card.tsx
@@ -8,10 +8,18 @@ interface Props {
 const Card: React.FC<Props> = ({ serviceName, image }) => {
   
   return (
-    <div className="flex flex-col max-w-[200px]">
-      <Image className="object-cover bg-slate-300" height={200} width={200} src={image} alt="servicio de tarot" />
-      <h4 className="text-xl font-semibold my-2 text-center h-16">{serviceName}</h4>
-    </div>
+<div
+  className="block overflow-hidden group"
+>
+  <Image 
+    className="bg-slate-300 group-hover:scale-105 transition-transform duration-500 object-cover w-full h-[350px] sm:h-[450px]" 
+    height={200} 
+    width={200} 
+    src={image} 
+    alt="servicio de tarot" 
+  />
+  <h4 className="text-xl font-semibold my-2 text-center h-16">{serviceName}</h4>
+</div>
   )
 }
 
